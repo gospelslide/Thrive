5ab98b7ded2ba99c7a93e476f2497fe7865521dc
yash shah <shahyash96.ys@gmail.com>
2016-09-23 15:28:37 +0530

    Route,account and transactions.

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,24 @@ Route::get('edit_stocks', function(){
 Route::get('/insert_stocks', 'StocksController@insertStocks');
 Route::post('/add_stocks', 'StocksController@addStocks');
 Route::get('/stocks_current', 'StocksController@getCurrentPrice');
+
+//Transaction Routes
+Route::get('/history', 'TransactionController@transaction_history');
+Route::get('/queue', 'TransactionController@transaction_queue');
+
+//Liquidity Routes
+Route::get('/manage', 'LiquidityController@surplus');
+
+//Account Routes
+Route::get('/add_saving', 'AccountController@add_saving');
+Route::post('/edit_saving', 'AccountController@edit_saving');
+Route::post('/register_saving', 'AccountController@register_saving');
+
+Route::get('/add_current', 'AccountController@add_current');
+Route::get('/edit_current', 'AccountController@edit_current');
+Route::post('/update_current', 'AccountController@update_current');
+Route::post('/register_current', 'AccountController@register_current');
+
+Route::get('/add_credit', 'AccountController@add_credit');
+Route::post('/edit_credit', 'AccountController@edit_credit');
+Route::post('/register_credit', 'AccountController@register_credit');
