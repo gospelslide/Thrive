2b19eda47800486a745c89cec2cf34222b7c10c9
Vishal Sheth <vishsheth5@gmail.com>
2016-09-25 09:31:08 +0530

    View account homepage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,13 +27,16 @@ class HomeController extends Controller
     public function index()
     {
         $user = Auth::user();
-        $credit_accounts = DB::table('credit_account')->
-                            where('customer_id', $user['attributes']['id'])->get();
-        $savings_accounts = DB::table('saving_account')->
-                            where('customer_id', $user['attributes']['id'])->get();
-        $current_accounts = DB::table('current_account')->
-                            where('customer_id', $user['attributes']['id'])->get();
-        return view('home', compact('credit_accounts', 'savings_accounts', 'current_accounts'));
+        $credit_accounts = DB::table('credit_account')
+                            ->join('bank', 'credit_account.bank_id', '=', 'bank.id')
+                            ->where('customer_id', $user['attributes']['id'])->get();
+        $savings_accounts = DB::table('saving_account')
+                            ->join('bank', 'saving_account.bank_id', '=', 'bank.id')
+                            ->where('customer_id', $user['attributes']['id'])->get();
+        $current_accounts = DB::table('current_account')
+                            ->join('bank', 'current_account.bank_id', '=', 'bank.id')
+                            ->where('customer_id', $user['attributes']['id'])->get();
+        return view('account.view_current', compact('credit_accounts', 'savings_accounts', 'current_accounts'));
     }
 
 }
